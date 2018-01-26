f2d5e499bfc6e9a1153f32670e354d90b2a3c89d
125m125 <125m125@125m125.de>
2018-01-27 00:07:57 +0100

    treat strings containing dots as paths for cli tools

--- src/cli/cliBuild.js
+++ src/cli/cliBuild.js
@@ -9,7 +9,7 @@ export default function cliBuild(arg) {
     if (!arg) {
         return console.log("buildoperation requires a path to an existing model or a name for the new model");
     }
-    if (arg.indexOf("/") !== -1) {
+    if (arg.indexOf("/") !== -1 || arg.indexOf(".") !== -1) {
         arg = new JSONSerializer().deserializeModel(JSON.parse(readFileSync(arg, "utf8")));
     }
     new InquirerBuilder(arg, true).start().catch(console.log);
